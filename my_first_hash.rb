c9de294867272d3ee9d2cb7fc32af99e3cef1e4a
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-31 06:22:13 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,9 +1,8 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  your_hash = {}
-  your_name["age"] = 5
-  your_hash[:name] = "Joaquin"
-  your_name["hometown"] = "Ithaca"
+  # your_hash = {}
+  your_name = {"age" => 5, :name => "Joaquin", "hometown" => "Ithaca"}
+  
 end
 
 
